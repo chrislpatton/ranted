fc86a1f70c46c0e2035d2b2d17b7dd8f701f2c17
Christopher Patton <chrislpatton@gmail.com>
2016-02-16 21:35:02 -0500

    didnt finsh

--- public/js/app.js
+++ public/js/app.js
@@ -2,32 +2,55 @@
 
 var rantedApp = angular.module('rantedApp', []);
 
-rantedApp.controller('AppCtrl', ['$scope', '$http',
-function($scope, $http){
+rantedApp.factory('messages', [function(){
+  var msg = {
+    
+        messages :[
+	ranter1 = {
+		username: "anon",
+		rant: "I soooo hate traffic",
+		thumbs: 15,
+		comments:"ME TOOO!"
+	},
+
+	ranter2 = {
+		username: "anon",
+		rant: "I cant stand people that rant lol",
+		thumbs: 3,
+		comments: ""
+	},
+
+	ranter3 = {
+		username: "anon",
+		rant: "Can't stand when my neighbors dog barks while I am asleep, I got to work in the morning",
+		thumbs: 1,
+		comments: ""
+	}
+	]
+  };
+  return msg;
+}])
+
+
+        // rantedApp.controller('CommentCtrl',['$scope', function ($scope) {
+        //     //This will hide the DIV by default.
+        //     $scope.IsVisible = false;
+        //     $scope.ShowHide = function () {
+        //         //If DIV is visible it will be hidden and vice versa.
+        //         $scope.IsVisible = $scope.IsVisible ? false : true;
+        //    }
+        //    ]);
+
+              
+
+rantedApp.controller('AppCtrl', ['$scope', '$http','messages',
+function($scope, $http, messages){
 
 	// $http.get('/messages').success(function(response){
 	// 	console.log("recieved the data requested");
 	// 	$scope.messages = response;
 	// })
-	 $scope.messages = [
-	ranter1 = {
-		username: "theRanter",
-		rant: "I soooo hate traffic",
-		thumbs: 0
-	},
-
-	ranter2 = {
-		username: "hater",
-		rant: "I cant stand people that rant lol",
-		thumbs: 3
-	},
-
-	ranter3 = {
-		username: "whyRant",
-		rant: "Can't stand when my neighbors dog barks while I am asleep, I got to work in the morning",
-		thumbs: 1
-	}
-	];
+	 $scope.messages = messages.messages;
 	  
 	  $scope.addPost = function(){
     	if($scope.rantmsg === '') { return; }
@@ -39,10 +62,18 @@ function($scope, $http){
     	$scope.rantmsg = '';
     
   };
-		console.log($scope.aRant);
+		$scope.addVote = function(message) {
+    	message.thumbs += 1;
+  		};
 	
-	
+	$scope.IsVisible = false;
+	$scope.ShowHide = function () {
+                //If DIV is visible it will be hidden and vice versa.
+    $scope.IsVisible = $scope.IsVisible ? false : true;
+     };
 
 	
 }
 ]);
+
+
